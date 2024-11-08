24fe406d1d61034c54ddc68a6e94d021a2fa5e7d
vanshitaa825 <vanshita.dixit@grazitti.com>
2024-11-08 17:58:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("howToPlayBtn").addEventListener("click", function() {
+    window.location.href = "index2.html"; 
+});
+
+
+document.getElementById("backButton").addEventListener("click", function() {
+    history.back(); 
+});
